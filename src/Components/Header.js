df27fc252428344576755c6b027e4cbd05cd2d94
Marco Giannini <gianemi2@gmail.com>
2022-03-08 13:06:48 +0100

    Div around ads

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,6 +8,7 @@ function Header() {
     useEffect(() => {
         if (adActive) {
             try {
+                console.log('Loading ads by google')
                 var adsbygoogle = (window.adsbygoogle || [])
                 adsbygoogle.push({})
             } catch (error) {
@@ -17,13 +18,15 @@ function Header() {
         }
     }, [adActive])
     return (
-        <div className='Header' style={{ textAlign: 'center' }}>
-            {
-                adActive ? <ins className="adsbygoogle"
-                    style={{ display: "block", width: "350px", height: "90px" }}
-                    data-ad-client="ca-pub-8441432984375470"
-                    data-ad-slot="2777106929"></ins> : false
-            }
+        <div className='Header' style={{ textAlign: 'center', width: "100%" }}>
+            <div className="banner" style={{ width: 350 }}>
+                {
+                    adActive ? <ins className="adsbygoogle"
+                        style={{ display: "block", width: "350px", height: "90px" }}
+                        data-ad-client="ca-pub-8441432984375470"
+                        data-ad-slot="2777106929"></ins> : false
+                }
+            </div>
             <img src={logo} style={{ maxWidth: 200 }} />
         </div>
     )
