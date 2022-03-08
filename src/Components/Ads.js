ef695c7a79ff203e589fe7ba509291e725ca7006
Marco Giannini <gianemi2@gmail.com>
2022-03-08 13:18:16 +0100

    Separated in two components ads

---
+++ src/Components/Ads.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import useLocalStorage from '../Hooks/useLocalStorage';
+
+const Ads = () => {
+    const [adActive] = useLocalStorage('adActive', false)
+
+    useEffect(() => {
+        try {
+            console.log(`adactive: ${adActive}`);
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+        } catch (error) {
+            console.log('error with ads');
+            console.log(error);
+        }
+    }, [adActive])
+
+    return (
+        <ins className="adsbygoogle"
+            style={{ display: "block", width: "350px", height: "90px" }}
+            data-ad-client="ca-pub-8441432984375470"
+            data-ad-slot="2777106929"></ins>
+    );
+
+}
+export default Ads
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,31 +1,12 @@
 import React, { useEffect } from 'react'
 import useLocalStorage from '../Hooks/useLocalStorage';
 import logo from '../logo.png';
-
+import Ads from './Ads';
 
 function Header() {
-    const [adActive] = useLocalStorage('adActive', false)
-    useEffect(() => {
-        if (adActive) {
-            try {
-                console.log('Loading ads by google')
-                window.adsbygoogle.push({})
-            } catch (error) {
-                console.log('error with ads');
-                console.log(error);
-            }
-        }
-    }, [adActive])
     return (
         <div className='Header' style={{ textAlign: 'center', width: "100%" }}>
-            <div className="banner" style={{ width: 350 }}>
-                {
-                    adActive ? <ins className="adsbygoogle"
-                        style={{ display: "block", width: "350px", height: "90px" }}
-                        data-ad-client="ca-pub-8441432984375470"
-                        data-ad-slot="2777106929"></ins> : false
-                }
-            </div>
+            <Ads></Ads>
             <img src={logo} style={{ maxWidth: 200 }} />
         </div>
     )
